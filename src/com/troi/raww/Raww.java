5381586328b464840ad3f3c50d572fce45fdfe8e
Troi-Ryan R. S <troiryan2@gmail.com>
2016-03-06 17:56:11 -0800

    future proofing

--- src/com/troi/raww/Raww.java
+++ src/com/troi/raww/Raww.java
@@ -1,8 +1,21 @@
 package com.troi.raww;
 
-public class Raww {
+public class Raww implements Runnable {
+
+  public static final String VERSION = "0.0.1a";
+
+  private static Raww instance;
+
+  @Override
+  public void run() {
+    System.out.println("Hello World");
+  }
 
   public static void main(String[] args) {
-    
+    (new Thread((instance = new Raww()))).start();
+  }
+
+  public static Raww getInstance() {
+    return instance;
   }
 }
