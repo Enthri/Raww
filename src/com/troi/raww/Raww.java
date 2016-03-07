1461af1f7c91e3499fda514d6f6ef75cc54cf9e6
Troi-Ryan R. S <troiryan2@gmail.com>
2016-03-06 18:29:47 -0800

    filehandler instance

--- src/com/troi/raww/Raww.java
+++ src/com/troi/raww/Raww.java
@@ -1,13 +1,17 @@
 package com.troi.raww;
 
+import com.troi.raww.io.FileHandler;
+
 public class Raww implements Runnable {
 
   public static final String VERSION = "0.0.1a";
-
   private static Raww instance;
+
+  private FileHandler fileHandler;
 
   @Override
   public void run() {
+    this.fileHandler = new FileHandler();
     System.out.println("Hello World");
   }
 
