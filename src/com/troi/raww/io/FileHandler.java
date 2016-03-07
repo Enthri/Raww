af67a701d07c88e8891197da7f315144331f5fc6
Troi-Ryan R. S <troiryan2@gmail.com>
2016-03-06 18:26:31 -0800

    io development

--- src/com/troi/raww/io/FileHandler.java
+++ src/com/troi/raww/io/FileHandler.java
@@ -4,24 +4,36 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class FileHandler {
 
-  public static void readFile(File... files) {
+  private final ArrayList<RawwFile> fileList;
+
+  public FileHandler() {
+    this.fileList = new ArrayList<RawwFile>();
+  }
+
+  public void readFiles(String... fileNames) {
     BufferedReader read = null;
-    for(File file : files) {
+    for(String fileName : fileNames) {
+      File file = new File(fileName);
+      if(!file.exists()) continue;
+      RawwFile rawwFile = new RawwFile(fileName);
       try {
         read = new BufferedReader(new FileReader(file));
         String line;
-        if(file.exists()) {
-          while((line = read.readLine()) != null) {
-          }
-        }
+        while((line = read.readLine()) != null) rawwFile.addLine(line);
       } catch(Exception e) {
       } finally {
         try { if(read != null) read.close();
         } catch(IOException e) { }
       }
+      this.fileList.add(rawwFile);
     }
+  }
+
+  public ArrayList<RawwFile> getFileList() {
+    return this.fileList;
   }
 }
--- src/com/troi/raww/io/RawwFile.java
+++ src/com/troi/raww/io/RawwFile.java
@@ -1,8 +1,30 @@
 package com.troi.raww.io;
 
+import java.util.ArrayList;
+
 public class RawwFile {
 
-  public RawwFile() {
+  private final String name;
+  private final ArrayList<String> lines;
 
+  public RawwFile() {
+    this("missing_file");
+  }
+
+  public RawwFile(String name) {
+    this.name = name;
+    this.lines = new ArrayList<String>();
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public ArrayList<String> getLines() {
+    return this.lines;
+  }
+
+  public void addLine(String str) {
+    this.lines.add(str);
   }
 }
