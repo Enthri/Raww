9e4d1182359f45e07db3fe2638472facf7bc08cc
Troi-Ryan R. S <troiryan2@gmail.com>
2016-03-06 20:31:57 -0800

    logic handler

--- src/com/troi/raww/Raww.java
+++ src/com/troi/raww/Raww.java
@@ -1,6 +1,7 @@
 package com.troi.raww;
 
 import com.troi.raww.io.FileHandler;
+import com.troi.raww.logic.LogicHandler;
 
 public class Raww implements Runnable {
 
@@ -8,12 +9,14 @@ public class Raww implements Runnable {
   private static Raww instance;
 
   private FileHandler fileHandler;
+  private LogicHandler logicHandler;
 
   @Override
   public void run() {
     this.fileHandler = new FileHandler();
     this.fileHandler.readFiles("test.raww");
-    System.out.println(fileHandler.getFileList().get(0).getLines().get(0));
+    this.logicHandler = new LogicHandler(fileHandler.getFileList());
+    this.logicHandler.run();
   }
 
   public static void main(String[] args) {
--- src/com/troi/raww/logic/LogicHandler.java
+++ src/com/troi/raww/logic/LogicHandler.java
@@ -1,5 +1,17 @@
 package com.troi.raww.logic;
 
+import com.troi.raww.io.RawwFile;
+import java.util.ArrayList;
+
 public class LogicHandler {
 
+  private ArrayList<RawwFile> files;
+
+  public LogicHandler(ArrayList<RawwFile> files) {
+    this.files = files;
+  }
+
+  public void run() {
+    System.out.println(files.get(0).getLines().get(0));
+  }
 }
